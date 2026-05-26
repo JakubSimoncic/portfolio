25e2196c3b1fad26fa8c29ac404f1e650cd2045c
JakubSimoncic <201454354+JakubSimoncic@users.noreply.github.com>
2026-05-26 18:34:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,19 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     links.forEach(link => {
         const href = link.getAttribute('href');
-        // Kontrola, zda odkaz obsahuje název souboru z cesty, nebo zda jsme na hlavní stránce
-        if (currentPath === href || (currentPath === "" && href === "index.html") || (currentPath === "index.html" && href === "index.html")) {
+        if (currentPath === href || (currentPath === "" && href === "index.html")) {
             link.classList.add('active');
         }
     });
-
-    // 3. Kosmetické přepsání URL (odstranění .html a přepsání indexu na home)
-    const url = window.location.href;
-    if (url.endsWith('index.html')) {
-        const cistaUrl = url.replace('index.html', 'home');
-        window.history.replaceState({}, '', cistaUrl);
-    } else if (url.endsWith('.html')) {
-        const cistaUrl = url.substring(0, url.length - 5); // Odřízne posledních 5 znaků (tj. ".html")
-        window.history.replaceState({}, '', cistaUrl);
-    }
 });
